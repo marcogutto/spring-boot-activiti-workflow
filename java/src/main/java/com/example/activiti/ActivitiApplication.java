3623ad75c582416a2975a7a6901b684a3cc515db
Marco Seabra <marcogutto@gmail.com>
2019-07-10 13:43:14 -0300

    Configuração da infra das classes

--- java/src/main/java/com/example/activiti/ActivitiApplication.java
+++ java/src/main/java/com/example/activiti/ActivitiApplication.java
@@ -1,13 +1,26 @@
 package com.example.activiti;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.activiti.service.EmployeeService;
 
 @SpringBootApplication
-public class ActivitiApplication {
-
+public class ActivitiApplication{
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ActivitiApplication.class, args);
 	}
+	
+	@Bean
+	public CommandLineRunner init(final EmployeeService employeeService) {
 
+		return new CommandLineRunner() {
+			public void run(String... strings) throws Exception {
+				
+			}
+		};
+	}
 }
